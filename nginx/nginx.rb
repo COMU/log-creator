1fddb33ef43e85a8b878f4ce238a7d3193a0be84
melekkahraman <melekkah12@gmail.com>
2013-12-20 01:39:46 +0200

    Update nginx.rb

--- nginx/nginx.rb
+++ nginx/nginx.rb
@@ -11,15 +11,13 @@
  while $x<array[0]
    File.open('access.log', 'w') do |f1|
    f1.print ip_rand,"--",time,dir_random,number_rand,"\"-\"",scanner
-   File.open('access.log', 'w') do |f2|
-   f2.print ip_rand,"--",time,errorcode,"\"-\"","\"-\""
+   f1.print ip_rand,"--",time,errorcode,"\"-\"","\"-\""
    $x=$x+1
  end
  $y=0
  while $y<array[1]
-   File.open('errorlog.rb', 'w') do |f3|
-   f3.print time,"(server.c.",number_rand,") server stopped by UID = 0",port
-   File.open('errorlog.rb', 'w') do |f4|
-   f4.print time,"(log.c.",number_rand,")server started"
+   File.open('errorlog.rb', 'w') do |f2|
+   f2.print time,"(server.c.",number_rand,") server stopped by UID = 0",port
+   f2.print time,"(log.c.",number_rand,")server started"
    $y=$y+1
  end

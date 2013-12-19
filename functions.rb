77e21da970caa7002c32cf00d5b7f281ed00ce28
melekkahraman <melekkah12@gmail.com>
2013-12-19 14:57:08 +0200

    Update functions.rb

--- functions.rb
+++ functions.rb
@@ -91,12 +91,6 @@ end
     array2=[100,101,200,202,203,204,205,206,300,301,302,303,304,305,307,400,401,403,404,405,406,407,408,409,410,411,412,413,414,415,416,417,500,501,502,503,505]
     puts array[rand(0..36)]
  end
-#generated random time
-  def time_random
-    date1=Time.now
-    date2=date1+$i
-    $i=$i+10.0
-    return Time.at(rand(date1..date2))
-  end
+
     
   

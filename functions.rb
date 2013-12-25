cc6cf4bccc0cb4ef27dc3730eea0e8286c6f90f4
Gulsah Kose <gulsah.1004@gmail.com>
2013-12-25 05:45:11 +0200

    added timeCreator,domainCreator,requestCreator

--- functions.rb
+++ functions.rb
@@ -11,6 +11,14 @@ class Parts
   def time(start_time, end_time)
     next_time = time_rand start_time, end_time
   end
+  def timeCreator
+    five_second_later = $now+5
+    int_now=$now.to_i 
+    int_five_second_later=five_second_later.to_i
+    random_time=Time.at(rand(int_now...int_five_second_later))
+    $now = five_second_later
+    return random_time
+  end
 
 #generat random ip.format:'115.232.14.162'
   def ip_rand
@@ -102,5 +110,17 @@ class Parts
   def error_code
     array=[100,101,200,202,203,204,205,206,300,301,302,303,304,305,307,400,401,403,404,405,406,407,408,409,410,411,412,413,414,415,416,417,500,501,502,503,505]
     puts array[rand(0..36)]
+  end
+#generated random request from list
+  def requestCreator 
+    requests=Array.[]('OPTIONS','GET','HEAD','POST','PUT','DELETE','TRACE','CONNECT')
+    rand_request=rand(0...requests.length)
+    return requests[rand_request]
+  end
+#generated real domain list
+  def domainCreator
+    domains=['https://www.google.com.tr/','http://members.comu.edu.tr/','http://www.akvaryum.com/','http://www.youtube.com/','https://www.facebook.com/','http://obs.comu.edu.tr/','https://github.com/','http://www.hurriyet.com.tr/','http://tureng.com/','http://www.tutorialspoint.com/']
+    rand_domain=rand(0...domains.length)
+    return domains[rand_domain]
   end
 end

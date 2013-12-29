ade53f3178b81d963025b8f19f838b2de4b001cc
aylincsknpnr <aylin_comu@hotmail.com>
2013-12-29 22:46:10 +0200

    Update lighttpd.rb
    Yazım hataları ve function.rb dosyasına göre fonksiyon isimleri düzeltildi .

--- lighttpd/lighttpd.rb
+++ lighttpd/lighttpd.rb
@@ -10,18 +10,18 @@ item1=line[1.to_i]
 k=0
 while k<item
   File.open('access.log', 'w') do |file1|
-  file1.print ip_rand,"--",timestamp,dir_random,number_rand,"\"-\"",browser_type
+  file1.print ip_rand,"--",time_rand,dir_random,number_rand,"\"-\"",browser_type
   File.open('access.log', 'w') do |file2|
-  file2.print ip_random,"--",timestamp,browser_type,"\"-\"","\"-\""
+  file2.print ip_rand,"--",time_rand,browser_type,"\"-\"","\"-\""
   k=k+1
 end
 #error log 
 j=0
 while j<item1
   File.open('error.log', 'w') do |file3|
-  file3.print timestamp,"(server.c.1546) server stopped by UID = 0",pid_random
+  file3.print time_rand,"(server.c.",number_rand,") server stopped by UID = 0",port
   File.open('error.log', 'w') do |file4|
-  file2.print timestamp,"(log.c.166) server started"
+  file4.print time_rand ,"(log.c.",number_rand,")server started"
   j=j+1
 end
 

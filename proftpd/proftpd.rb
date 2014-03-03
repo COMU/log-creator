cdf0d6a207f292a7dfd18346ca0cb1af8a5c118a
Melek KAHRAMAN <melekkah12@gmail.com>
2014-03-03 21:04:44 +0200

    proftpd log

---
+++ proftpd/proftpd.rb
@@ -0,0 +1,16 @@
+load '../functions.rb'
+array=Array.new
+line=""
+pot=IO.readlines("data.txt")
+array=pot[1]
+array=line.split(" ")
+$i=0
+$a=word_rand
+while $i<array[0]
+   File.open('proftpd.log', 'w') do |f1|
+   if $i<1
+     f1.print time_rand,a,"proftpd","[",number_rand,"]",a,"(",word_rand,":","ProFTPD 1.3.4a (maint) (built Fri Apr 19 2013 20:25:01 UTC) standalone mode STARTUP"
+   end
+   f1.print time_rand,a,"proftpd","[",number_rand,"]",a,"(",word_rand,"[",ip_rand,"]):",connection_rand
+   $i=$i+1
+end

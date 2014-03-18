12c2cd33c27359a218eefb22b2db2fc2d8d94e0e
melekkahraman <melekkah12@gmail.com>
2014-03-19 00:10:24 +0200

    Update proftpd.rb

--- proftpd/proftpd.rb
+++ proftpd/proftpd.rb
@@ -5,12 +5,12 @@ pot=IO.readlines("data.txt")
 array=pot[1]
 array=line.split(" ")
 $i=0
-$a=word_rand
+$a=computer_rand
 while $i<array[0]
    File.open('proftpd.log', 'w') do |f1|
    if $i<1
      f1.print time_rand,a,"proftpd","[",number_rand,"]",a,"(",word_rand,":","ProFTPD 1.3.4a (maint) (built",time_rand,"UTC) standalone mode STARTUP"
    end
-   f1.print time_rand,a,"proftpd","[",number_rand,"]",a,"(",word_rand,"[",ip_rand,"]):",connection_rand
+   f1.print time_rand,a,"proftpd","[",number_rand,"]",a,"(",address,"):",connection_rand
    $i=$i+1
 end

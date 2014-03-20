7a4472e58f7251e3642bed695314a863689e7ebe
aylincsknpnr <aylin_comu@hotmail.com>
2014-03-20 02:27:51 +0200

    Update vsftplog.rb

--- vsftp/vsftplog.rb
+++ vsftp/vsftplog.rb
@@ -6,10 +6,15 @@ line1=arr[0].split("=")
 line2=arr[1].split("=")
 line1[1]=line1[1].to_i
 line2[1]=line2[1].to_i
+#line3=Array.new
+#j=Array.new
+#line3=arr[2].split("=")
+#j=line3[1].split(",")
 #vsftpd logs
 k=0
 while k<line1[1]
   $a=line2[rand(0..line2[1])]
+  #$b=j[rand(0..j.length)]
   #Connection attempt:
   puts time_rand, '[pid ',number_rand,']','CONNECT: Client " ',ip_rand,'"'
   #Failed login:
@@ -19,7 +24,7 @@ while k<line1[1]
   #Anonymous login :
   puts time_rand,'[pid',number_rand,']','[ftp] OK LOGIN : Client "',ip_rand , 'anon password "',word_rand,'@"'
   #File upload :
-  puts time_rand,'[pid',number_rand,']','[xx] OK UPLOAD : Client "',ip_rand,'"/',word_rand,'.',extension,a,'MB"'
+  puts time_rand,'[pid',number_rand,']','[xx] OK UPLOAD : Client "',ip_rand,'"/',word_rand,'.',extension,a,'MB"'#extension yerine $b yazdırılabilir
   k=k+1
 end
     

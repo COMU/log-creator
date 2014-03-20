d0c084d95a5c3800c42c60aa808876190958f16b
aylincsknpnr <aylin_comu@hotmail.com>
2014-03-20 13:53:07 +0200

    Update vsftplog.rb

--- vsftp/vsftplog.rb
+++ vsftp/vsftplog.rb
@@ -11,20 +11,20 @@ line2[1]=line2[1].to_i
 #line3=arr[2].split("=")
 #j=line3[1].split(",")
 #vsftpd logs
+func = Parts.new
 k=0
 while k<line1[1]
-  $a=line2[rand(0..line2[1])]
+  $a=rand(0..line2[1])
   #$b=j[rand(0..j.length)]
   #Connection attempt:
-  puts time_rand, '[pid ',number_rand,']','CONNECT: Client " ',ip_rand,'"'
+  print func.time_rand, '[pid ',func.number_rand,']','CONNECT: Client " ',func.ip_rand,'"'
   #Failed login:
-  puts time_rand,'[pid',number_rand,']','FAIL LOGIN : Client "',ip_rand,'"'
+  print func.time_rand,'[pid',func.number_rand,']','FAIL LOGIN : Client "',func.ip_rand,'"'
   #Login ok:
-  puts time_rand,'[pid',number_rand,']','[dcid] OK LOGIN : Client "',ip_rand
+  print func.time_rand,'[pid',func.number_rand,']','[dcid] OK LOGIN : Client "',func.ip_rand,'"'
   #Anonymous login :
-  puts time_rand,'[pid',number_rand,']','[ftp] OK LOGIN : Client "',ip_rand , 'anon password "',word_rand,'@"'
+  print func.time_rand,'[pid',func.number_rand,']','[ftp] OK LOGIN : Client "',func.ip_rand , 'anon password "',func.word_rand,'@"'
   #File upload :
-  puts time_rand,'[pid',number_rand,']','[xx] OK UPLOAD : Client "',ip_rand,'"/',word_rand,'.',extension,a,'MB"'#extension yerine $b yazdırılabilir
+  print func.time_rand,'[pid',func.number_rand,']','[xx] OK UPLOAD : Client "',func.ip_rand,'"/',func.word_rand,'.',func.extension,' ',$a,' ', 'MB"'#extension yerine $b yazdırılabilir
   k=k+1
 end
-    

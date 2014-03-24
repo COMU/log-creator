895569d814bbba7c09547f5b1b23d980dea61aca
aylincsknpnr <aylin_comu@hotmail.com>
2014-03-24 16:34:00 +0200

    Update lighttpd.rb

--- lighttpd/lighttpd.rb
+++ lighttpd/lighttpd.rb
@@ -6,22 +6,23 @@ read=arr[1]
 line=read.split(" ")
 item=line[0].to_i
 item1=line[1].to_i
+func = Parts.new
 #access log 
 k=0
 while k<item
   File.open('access.log', 'w') do |file1|
-  file1.print ip_rand,"--",time_rand,dir_random,number_rand,"\"-\"",browser_type
+  file1.print func.ip_rand,"--",func.time_rand,func.dir_random,func.number_rand,"\"-\"",func.browser_type
   File.open('access.log', 'w') do |file2|
-  file2.print ip_rand,"--",time_rand,browser_type,"\"-\"","\"-\""
+  file2.print func.ip_rand,"--",func.time_rand,func.browser_type,"\"-\"","\"-\""
   k=k+1
 end
 #error log 
 j=0
 while j<item1
   File.open('error.log', 'w') do |file3|
-  file3.print time_rand,"(server.c.",number_rand,") server stopped by UID = 0",port
+  file3.print func.time_rand,"(server.c.",func.number_rand,") server stopped by UID = 0",func.port
   File.open('error.log', 'w') do |file4|
-  file4.print time_rand ,"(log.c.",number_rand,")server started"
+  file4.print func.time_rand ,"(log.c.",func.number_rand,")server started"
   j=j+1
 end
 

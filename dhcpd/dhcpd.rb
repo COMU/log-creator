19bdc2fbd7eba6581689f7009a841e30b3bfe2e2
melekkahraman <melekkah12@gmail.com>
2014-04-13 22:53:47 +0300

    Update dhcpd.rb

--- dhcpd/dhcpd.rb
+++ dhcpd/dhcpd.rb
@@ -4,20 +4,31 @@ func=Parts.new
 array=Array.new
 show=Array.new
 show1=Array.new
+show2=Array.new
 array=pot[0]
 show=array.split("=")
 show[1]=show[1].to_i
 $i=0
 while $i<show[1]
-  array=pot[1]
+  array=pot[3]
   show1=array.split(",")
+  $d=func.time_rand
+  $d=$d.to_i
+  array=pot[2]
+  show2=array.split("=")
+  show2[1]=show2[1].to_i
+  later=show2[1]+ $d
+  result=Time.at(later)
+  $d=Time.at($d)
   $b=show1[rand(0..show1.length)]
   $a=rand(1..10)
-  print "lease"," ",func.ip_rand,"{"," ","\n"
-  print "          ","starts"," ",$a," ",func.time_rand,";","\n"
-  print "          ","ends"," ",$a," ",func.time_rand,";","\n"
-  print "          ","hardware ethernet"," ",func.mac_address,";","\n"
-  print "          ","uid"," ","01:",func.mac_address,";","\n"
+  $c=192.168
+  $e=rand(0..255)
+  print "lease"," ",func.ip_rand," ","{"," ","\n"
+  print "          ","starts"," ",$a," ",$d,";","\n"
+  print "          ","ends"," ",$a," ",result,";","\n"
+  print "          ","hardware ethernet"," ",$c,".1.",$e,";","\n"
+  print "          ","uid"," ","01",":",$c,".1.",$e,";","\n"
   print "          ","client-hostname"," ",'"',$b,'"',";","\n"
   print "}","\n"
   $i=$i+1

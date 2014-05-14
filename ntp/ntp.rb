59576c2637c4aa91f179452298610bee1456cc0e
aylincsknpnr <aylin_comu@hotmail.com>
2014-05-14 23:21:06 +0300

    Update ntp.rb

--- ntp/ntp.rb
+++ ntp/ntp.rb
@@ -6,13 +6,16 @@ line1=arr[0].split("=")
 line1[1]=line1[1].to_i
 j=Array.new
 j=arr[2].split(",")
+arr2=["kernel time sync status change 2001 ",
+       "kernel time sync status 2040 "]
+#$c=arr2[rand(0..arr2.length)]
 #ntpd logs
 func = Parts.new
 k=0
 while k<line1[1]
   $b=j[rand(0..j.length)]
-  print " "
-  print func.time_rand," ",'ntpd[',func.number_rand,']:',$b," "
-  puts " "
+  print func.time_rand," ",'ntpd[',func.number_rand,']:',arr2[0],"\n "
+  print func.time_rand," ",'ntpd[',func.number_rand,']:',$b,"\n "
+  print func.time_rand," ",'ntpd[',func.number_rand,']:',arr2[1],"\n "
   k=k+1
 end
